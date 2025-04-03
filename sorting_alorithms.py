5bb6f12949caf5f077cbf1869eedf60829f5a3c4
Balavignesh-J <balav3230@gmail.com>
2025-04-03 12:27:40 +0530

    quick sort

--- sorting_alorithms.py
+++ sorting_alorithms.py
@@ -65,6 +65,29 @@ def merge(arr):
             rp+=1
             ap+=1
 
-arr=[5,1,4,2,3]
-merge(arr)
+def quick(arr,left,right):
+    if left < right:
+        pos = pivotpos(arr,left,right)
+        quick(arr,left,pos-1)
+        quick(arr,pos+1,right)
+
+def pivotpos(arr,left,right):
+    i=left
+    j=right-1
+    pivot=arr[right]
+    while i<j:
+        while i<right and arr[i]<pivot:
+            i+=1
+        while j>left and arr[j]>=pivot:
+            j-=1
+        if i<j:
+            arr[i],arr[j]=arr[j],arr[i]
+
+    if arr[i]>pivot:
+        arr[i],arr[right]=arr[right],arr[i]
+
+    return i
+
+arr=[5,1,4,2,3,6,8,7,9]
+quick(arr,0,len(arr)-1)
 print(arr)

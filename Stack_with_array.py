3078c622b8ec22da89304fecbea3bd7a4c720c95
Balavignesh J <balav3230@gmail.com>
2025-04-11 21:51:30 +0530

    stack with array

---
+++ Stack_with_array.py
@@ -0,0 +1,43 @@
+from Template import stack_template
+
+class S:
+    def __init__(self):
+        self.stack=[]
+        self.top=-1
+
+    def isempty(self):
+        if self.top!=-1:
+            return True
+        else:
+            return False
+
+    def push(self,n):
+        self.stack.append(n)
+        self.top+=1
+        print("Push success")
+
+    def pop(self):
+        if self.isempty():
+            n=self.stack.pop()
+            self.top-=1
+            print(f"The deleted element is {n}")
+        else:
+            print("stack is empty")
+
+    def peek(self):
+        if self.isempty():
+            n=self.stack[self.top]
+            print(f"The Top element is {n}")
+        else:
+            print("stack is empty")
+
+    def display(self):
+        if self.isempty():
+            for i in range(self.top,-1,-1):
+                print(f"{i+1} element is {self.stack[i]}")
+        else:
+            print("stack is empty")
+
+
+
+stack_template(S,"Stack with Array")
--- Template.py
+++ Template.py
@@ -20,3 +20,23 @@ def template(cls,name):
             ll.display()
         else:
             break
+
+def stack_template(cls,name):
+    s=cls()
+    print(f"Stack operations with {name}")
+    print("Menu")
+
+    while True:
+        print("1.Push \n2.Pop \n3.Peek \n4.Display")
+        n = int(input("Enter your choice: "))
+        if n == 1:
+            n = int(input("enter the value:"))
+            s.push(n)
+        elif n == 2:
+            s.pop()
+        elif n == 3:
+            s.peek()
+        elif n == 4:
+            s.display()
+        else:
+            break

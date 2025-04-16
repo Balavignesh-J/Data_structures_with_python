661003b4f0d54a79ba813b2bee258a7b4373d3fb
Balavignesh J <balav3230@gmail.com>
2025-04-16 16:50:10 +0530

    Update Template.py

--- Template.py
+++ Template.py
@@ -43,7 +43,7 @@ def stack_template(cls,name):
 
 
 def queue_template(cls,name):
-    s=cls()
+    q=cls()
     print(f"Queue operations with {name}")
     print("Menu")
 
@@ -52,12 +52,35 @@ def queue_template(cls,name):
         n = int(input("Enter your choice: "))
         if n == 1:
             n = int(input("enter the value:"))
-            s.enque(n)
+            q.enque(n)
         elif n == 2:
-            s.deque()
+            q.deque()
         elif n == 3:
-            s.peek()
+            q.peek()
         elif n == 4:
-            s.display()
+            q.display()
+        else:
+            break
+
+def dequeue_template(cls,name):
+    q=cls()
+    print(f"Queue operations with {name}")
+    print("Menu")
+    
+    print("1.Enque_front \n2.Deque_front \n3.Enque_rear \n4.Deque_rear \n5.Display")
+    while True:
+        n = int(input("Enter your choice: "))
+        if n == 1:
+            n = int(input("enter the value:"))
+            q.enque_front(n)
+        elif n == 2:
+            q.deque_front()
+        elif n == 3:
+            n = int(input("enter the value:"))
+            q.enque_rear(n)
+        elif n == 4:
+            q.deque_rear()
+        elif n==5:
+            q.display()
         else:
             break

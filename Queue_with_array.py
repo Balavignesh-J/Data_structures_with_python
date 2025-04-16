130d213204632df7b919277de413ba43de3dcd79
Balavignesh J <balav3230@gmail.com>
2025-04-16 16:49:14 +0530

    Update Queue_with_array.py

--- Queue_with_array.py
+++ Queue_with_array.py
@@ -1,34 +1,26 @@
-from Template import queue_template
-
 class Q:
     def __init__(self):
         self.queue=[]
-        self.front=0
-        self.rear=0
         self.size=10
-        self.count=0
 
     def isempty(self):
-        if self.front==self.rear:
+        if len(self.queue)==0:
             return True
         else:
             return False
     
     def qfull(self):
-        return self.count==self.size
+        return self.size==len(self.queue)
             
 
     def enque(self,n):
         if not self.qfull():       
             self.queue.append(n)
-            self.rear+=1
-            self.count+=1
             print("Enque success")
 
     def deque(self):
         if not self.isempty():
             n=self.queue.pop(0)
-            self.front+=1
             print(f"The deleted element is {n}")
         else:
             print("Queue is empty")
@@ -42,10 +34,29 @@ class Q:
 
     def display(self):
         if not self.isempty():
-            for i in range(self.front,self.rear):
-                print(f"{i+1} element is {self.queue[i]}")
+            for i,val in enumerate(self.queue):
+                print(f"{i+1} element is {val}")
         else:
             print("Queue is empty")
 
+def queue_template(cls,name):
+    q=cls()
+    print(f"Queue operations with {name}")
+    print("Menu")
+
+    while True:
+        print("1.Enque \n2.Deque \n3.Next delete \n4.Display")
+        n = int(input("Enter your choice: "))
+        if n == 1:
+            n = int(input("enter the value:"))
+            q.enque(n)
+        elif n == 2:
+            q.deque()
+        elif n == 3:
+            q.next_del()
+        elif n == 4:
+            q.display()
+        else:
+            break
 
 queue_template(Q,"Array")

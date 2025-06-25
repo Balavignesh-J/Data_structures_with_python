ab7e04ea73c48795e98e4a294a4571f725c993f5
Balavignesh J <balav3230@gmail.com>
2025-06-25 10:37:22 +0530

    bst implementation

--- Tree/binary_search_tree.py
+++ Tree/binary_search_tree.py
@@ -4,7 +4,7 @@ class Treenode:
         self.left=None
         self.right=None
 
-class BinaryTree:
+class BinarysearchTree:
     def __init__(self):
         self.root=None
 
@@ -15,13 +15,19 @@ class BinaryTree:
             return
         self.recursiveadd(node,self.root)
 
-    def recursiveadd(self,data,node):
-        if not node.left:
-            node.left=data
-        elif not node.right:
-            node.right=data
-        else:
-            self.recursiveadd(data,node.left)
+    def recursiveadd(self,new_data,node):
+        if not node.left and new_data.data<node.data:
+            node.left=new_data
+            return
+        elif not node.right and new_data.data>=node.data:
+            node.right=new_data
+            return
+
+        if node.left and new_data.data<node.data:
+            self.recursiveadd(new_data,node.left)
+        if node.right and new_data.data>=node.data:
+            self.recursiveadd(new_data,node.right)
+
 
     def display(self,depth=0,node=None):
         if not node:
@@ -31,7 +37,6 @@ class BinaryTree:
             self.display(depth+1,node.left)
         if node.right:
             self.display(depth+1,node.right)
-
     def traversal_display(self):
         inorder=[]
         preorder=[]
@@ -80,13 +85,14 @@ class BinaryTree:
         if node.left and node.left.data==data:
             node.left=None
             return
-        if node.right and node.right.data==data:
+        elif node.right and node.right.data==data:
             node.right=None
             return
-        if node.left:
+        elif data<node.data:
             self.recursiveremove(data,node.left)
-        if node.right:
+        elif data>=node.data:
             self.recursiveremove(data,node.right)
+
     def search(self,data):
         nodefound=self.findnode(data,self.root)
         if nodefound:
@@ -96,20 +102,22 @@ class BinaryTree:
     def findnode(self,data,node):
         if not node or node.data==data:
             return node
-        return self.findnode(data,node.left) or self.findnode(data,node.right)
+        if data<node.data:
+            return self.findnode(data,node.left)
+        elif data>=node.data:
+            return self.findnode(data,node.right)
 
 
-binaryTree = BinaryTree()
-binaryTree.add(5)
-binaryTree.add(1)
-binaryTree.add(2)
-binaryTree.add(3)
-binaryTree.add(4)
-binaryTree.add(5)
-binaryTree.display()
+binarysearchTree = BinarysearchTree()
+binarysearchTree.add(5)
+binarysearchTree.add(1)
+binarysearchTree.add(2)
+binarysearchTree.add(3)
+binarysearchTree.add(4)
+binarysearchTree.display()
 print("\n")
-binaryTree.traversal_display()
-binaryTree.remove(4)
-binaryTree.display()
+binarysearchTree.traversal_display()
+binarysearchTree.remove(4)
+binarysearchTree.display()
 print("\n")
-binaryTree.search(3)
+binarysearchTree.search(3)

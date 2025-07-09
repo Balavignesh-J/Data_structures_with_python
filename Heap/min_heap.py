744d7bd26e1882f4b4bd2c98b059cd168bb8d116
Balavignesh J <balav3230@gmail.com>
2025-07-10 00:38:41 +0530

    halfway through min heap

---
+++ Heap/min_heap.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self,data):
+        self.node=data
+        self.left=None
+        self.right=None
+
+class min_heap:
+    def __init__(self):
+        self.root=None
+
+    def add(self,n):
+        if not self.root:
+            self.root=Node(n)
+            return
+        self.recursiveadd(n,self.root)
+
+    def recursiveadd(self,n,node):
+        if not node.left:
+            node.left=Node(n)
+            self.heapify_up(node.left)
+        elif not node.right:
+            node.right=Node(n)
+            self.heapify_up(node.right)
+        else:
+            if self.get_count(node.left) > self.get_count(node.right):
+                node.right=Node(n)
+                self.heapify_up(node.left)
+            else:
+                node.left=Node(n)
+                self.heapify_up(node.right)
+
+    def get_count(self,node):
+        if not node:
+            return 0
+
+        return 1 + self.get_count(node.left) + self.get_count(node.right)
+
+    def heapify_up(self,node):
+        while node and node!=self.root:
+            parent=self.get_parent(node,self.root)
+            if parent.data > node.data:
+                parent.data,node.data=node.data,parent.data
+                node=parent
+            else:
+                break
+
+    def get_parent(self,node,root):
+        if root.left == node and root.right == node:
+            return root
+
+        if root.left:
+            parent=self.get_parent(node,root.left)
+            if parent:
+                return parent
+        if root.right:
+            parent=self.get_parent(node,root.right)
+            if parent:
+                return parent
+
+        return None
